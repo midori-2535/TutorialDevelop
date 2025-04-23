4edd55145fcb2b04163a408177aae3374458a675
midori-2535 <ya-ta_33@c-able.ne.jp>
2025-04-23 14:43:39 +0900

    User登録機能の作成

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -12,14 +12,21 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserService(UserRepository repository) {
-        this.userRepository =  repository;
+        this.userRepository = repository;
     }
 
-    /**全件を検索して返す*/
+    /** 全件を検索して返す */
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
 
+    // ----- 追加:ここから -----
+    /** Userの登録を行なう */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
 
 }
