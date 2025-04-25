6e8c9b4f4c392b83d6f59c0afda8f73cdbcfab3f
midori-2535 <ya-ta_33@c-able.ne.jp>
2025-04-25 23:54:22 +0900

    User削除機能の作成

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.techacademy.controller;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping; // 追加
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.techacademy.entity.User; // 追加
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
 @Controller
@@ -71,5 +74,12 @@ public class UserController {
         return "redirect:/user/list";
     }
 
+    /**User削除処理*/
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set <Integer>idck, Model model) {
+        // Userを一括削除
+            service.deleteUser(idck);
+            return "redirect:/user/list";
+    }
 
 }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // 追加
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -22,7 +24,6 @@ public class UserService {
     }
 
     /**Userを一件検索して返す*/
-    @Transactional
     public User getUser(Integer id) {
         return userRepository.findById(id).get();
     }
@@ -31,5 +32,13 @@ public class UserService {
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    /**Userの削除を行なう*/
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for (Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 }
